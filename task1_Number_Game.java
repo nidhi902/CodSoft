db85657ee668a6a3460bf1b97e6bc39fff2927f1
nidhi902 <nidhiprgm45@gmail.com>
2025-11-07 19:18:10 +0530

    working on task1

--- task1_Number_Game.java
+++ task1_Number_Game.java
@@ -1,4 +1,32 @@
-public class task1_Number_Game {
+import java.util.Random;
+import java.util.Scanner;
 
+public class task1_Number_Game {
+    public static void main(String[] args) {
+        Random rand=new Random();
+        Scanner sc=new Scanner(System.in);
+    
+        int guess=0;
+
+        int num=rand.nextInt(100)+1;
+        System.out.println("guess a number between 1 to 100:");
+        
+        while(guess!=num){
+            System.out.println("enter your guess:");
+            guess=sc.nextInt();
+            
+            if (guess==num){
+                System.out.println("your guess is correct!");
+            }
+            else if (guess>num){
+                System.out.println("your guess is to high");
+            }
+            else{
+                System.out.println("your guess is low");
+            }
+        }
+        
+
+    }
     
 }
